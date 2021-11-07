09393bd3048594947b39cf206ade9648814a6791
aymeneW <wiwi10072004@gmail.com>
2021-11-07 19:09:07 +0100

    first ios app hopfully:)

---
+++ android/app/src/main/kotlin/com/example/my_life_is_a_mess/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_life_is_a_mess
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: 
+        Text('First ios app test',style: TextStyle(fontWeight: FontWeight.bold),)
+        ,
+      ),
+    );
+  }
+}
